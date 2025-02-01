139e30b0ff2c925621b04126bbddb6c135dfada7
wsj2681 <wsj2681@naver.com>
2025-02-01 09:56:29 +0900

    오브젝트 식별자 구상

--- AgeofWar/AgeofWar/TitleScene.cpp
+++ AgeofWar/AgeofWar/TitleScene.cpp
@@ -1,6 +1,10 @@
 #include "TitleScene.h"
 #include "LobbyScene.h"
 #include <iostream>
+#include <functional>
+
+#define OBJECT_BEHAVIOR_BY_TYPE(x, y, z) if((x).substr(0, (y).size()) == (y)) { z; }
+#define OBJECT_BEHAVIOR_BY_NAME(x, y, z) if((x).substr((x).size() - (y).size(), (y).size()) == (y)) { z; }
 
 using namespace sf;
 
@@ -21,18 +25,28 @@ void TitleScene::Init()
 	auto font = new Font();
 	font->loadFromFile("Font/ChosunCentennial_ttf.ttf");
 	auto* text = new Text("Title Scene", *font, 50);
-	text->setPosition(400, 300);
+	text->setOrigin(text->getGlobalBounds().width / 2, text->getGlobalBounds().height / 2);
+	text->setPosition(window->getSize().x / 2, window->getSize().y / 2);
 
-	objects.push_back({text, "TEXT_object"});
+	objects.push_back({ text, "TEXT_character" });
+
+	auto called = [](const std::string& say)
+		{
+			std::cout << say << std::endl;
+		};
+
+	std::function<void(const std::string)> called2 = [](const std::string& say)
+		{
+			std::cout << say << std::endl;
+		};
+
 
 	for (auto& object : objects)
 	{
-		if (object.second.substr(0, 4) == "TEXT")
-		{
-			std::cout << "TEXT object Created" << std::endl;
-		}
+		OBJECT_BEHAVIOR_BY_NAME(object.second, std::string("character"), called("OBJECT_BEHAVIOR_BY_NAME called"));
+		OBJECT_BEHAVIOR_BY_TYPE(object.second, std::string("TEXT"), called("OBJECT_BEHAVIOR_BY_TYPE called"));
+		OBJECT_BEHAVIOR_BY_NAME(object.second, std::string("character"), called2("OBJECT_BEHAVIOR_BY_NAME called2"));
 	}
-
 }
 
 void TitleScene::UpdateInput(sf::Event& event)
